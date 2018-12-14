6dd8b05092b2eababe23679ff7dbc93f9f5375b8
Ryan <ryannaylor@hotmail.com>
2018-12-14 18:01:50 +0000

    Added Days 6 and 10

---
+++ AdventOfCode/AdventOfCode/2018/Day10.cs
@@ -0,0 +1,142 @@
+﻿using AdventOfCode.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2018
+{
+    class Day10 : DayProgram
+    {
+        public override void Run(string part)
+        {
+            var fileText = FileReader.ReadFile(2018, 10);
+            var nav = new StarNavigator(fileText);
+            int time = 0;
+            while (true)
+            {
+                nav.Tick();
+                if (nav.CheckGridSize())
+                {
+                    nav.Untick();
+                    nav.Output();
+                    Console.WriteLine(time);
+                    Console.ReadKey();
+                    
+                }
+                time++;
+            }
+        }
+
+        private class StarNavigator
+        {
+            private long previousArea = 0;
+            IList<Star> stars = new List<Star>();
+            string pattern = "^position=<(.*), (.*)> velocity=<(.*), (.*)>$";
+
+            public StarNavigator(string input)
+            {
+                Regex r = new Regex(pattern, RegexOptions.IgnoreCase);
+                //loop through the list of instructions and parse out the conditions
+                foreach (string instruction in input.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var m = r.Match(instruction);
+                    if (m.Success)
+                    {
+                        var star = new Star()
+                        {
+                            xPos = int.Parse(m.Groups[1].Value),
+                            yPos = int.Parse(m.Groups[2].Value),
+                            xVel = int.Parse(m.Groups[3].Value),
+                            yVel = int.Parse(m.Groups[4].Value)
+                        };
+                        stars.Add(star);
+                    }
+                }
+            }
+
+            public bool CheckGridSize()
+            {
+                //get the size grid we need
+                var left = stars.Min(x => x.xPos);
+                var top = stars.Max(x => x.yPos);
+                var right = stars.Max(x => x.xPos);
+                var bottom = stars.Min(x => x.yPos);
+
+                long width = right - left;
+                long height = top - bottom;
+
+                long currentArea = width * height;
+                var bigger = currentArea > previousArea && previousArea != 0;
+                previousArea = currentArea;
+                return bigger;
+            }
+
+            public void Tick()
+            {
+                foreach (Star star in stars)
+                {
+                    star.Move();
+                }
+            }
+
+            public void Untick()
+            {
+                foreach (Star star in stars)
+                {
+                    star.Reverse();
+                }
+            }
+
+            public void Output()
+            {
+                //get the size grid we need
+                var left = stars.Min(x => x.xPos);
+                var top = stars.Max(x => x.yPos);
+                var right = stars.Max(x => x.xPos);
+                var bottom = stars.Min(x => x.yPos);
+
+                for (int yGrid = bottom; yGrid <= top; yGrid++)
+
+                {
+                    var row = "";
+                    for (int xGrid = left; xGrid <= right; xGrid++)
+                    {
+                        if (stars.Where(x => x.xPos == xGrid && x.yPos == yGrid).Any())
+                        {
+                            row += "*";
+                        }
+                        else
+                        {
+                            row += ".";
+                        }
+                    }
+                    Console.WriteLine(">"+row);
+                }
+            }
+
+        }
+
+        private class Star
+        {
+            public int xPos = 0;
+            public int yPos = 0;
+            public int xVel = 0;
+            public int yVel = 0;
+
+            public void Move()
+            {
+                xPos += xVel;
+                yPos += yVel;
+            }
+
+            public void Reverse()
+            {
+                xPos -= xVel;
+                yPos -= yVel;
+            }
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode/2018/Day6.cs
@@ -0,0 +1,149 @@
+﻿using AdventOfCode.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2018
+{
+    class Day6 : DayProgram
+    {
+        public override void Run(string part)
+        {
+            var fileText = FileReader.ReadFile(2018, 6);
+            var nav = new DangerIdentifier(fileText);
+            Console.WriteLine(nav.FindSafeSpotB());
+        }
+
+        private class DangerIdentifier
+        {
+            Dictionary<Tuple<int, int>, int> locations = new Dictionary<Tuple<int, int>, int>();
+            string pattern = "^(.*), (.*)$";
+
+            public DangerIdentifier(string input)
+            {
+                Regex r = new Regex(pattern, RegexOptions.IgnoreCase);
+                //loop through the list of instructions and parse out the conditions
+                foreach (string instruction in input.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var m = r.Match(instruction);
+                    if (m.Success)
+                    {
+                        var location = new Tuple<int, int>(int.Parse(m.Groups[1].Value), int.Parse(m.Groups[2].Value));
+                        locations.Add(location, 0);
+                    }
+                }
+            }
+
+            public int FindSafeSpotA()
+            {
+                var left = locations.Min(x => x.Key.Item1);
+                var top = locations.Max(x => x.Key.Item2);
+                var right = locations.Max(x => x.Key.Item1);
+                var bottom = locations.Min(x => x.Key.Item2);
+
+                //first, exclude anything which is deemed 'infinite'
+                //take the first row / column outside the area, and determine the closest location
+                //that one will be infinite
+                var infiniteLocations = new List<Tuple<int, int>>();
+                for (int yGrid = bottom; yGrid <= top; yGrid++)
+                {
+                    foreach (int xGrid in new int[] { left, right })
+                    {
+                        var closestLocation = FindClosest(xGrid, yGrid);
+                        if (closestLocation != null)
+                        {
+                            infiniteLocations.Add(closestLocation);
+                        }
+                    }
+                }
+
+                for (int xGrid = left; xGrid <= right; xGrid++)
+                {
+                    foreach (int yGrid in new int[] { bottom, top })
+                    {
+                        var closestLocation = FindClosest(xGrid, yGrid);
+                        if (closestLocation != null)
+                        {
+                            infiniteLocations.Add(closestLocation);
+                        }
+                    }
+                }
+
+                //loop through everywhere in the grid and work out the distance to each location, then increment the closest one
+                for (int yGrid = bottom; yGrid <= top; yGrid++)
+                {
+                    for (int xGrid = left; xGrid <= right; xGrid++)
+                    {
+                        var closestLocation = FindClosest(xGrid, yGrid);
+                        if (closestLocation != null)
+                        {
+                            locations[closestLocation]++;
+                        }
+                    }
+                }
+                return locations.Where(loc => !infiniteLocations.Contains(loc.Key)).Max(x => x.Value);
+            }
+
+            public int FindSafeSpotB()
+            {
+                var left = locations.Min(x => x.Key.Item1);
+                var top = locations.Max(x => x.Key.Item2);
+                var right = locations.Max(x => x.Key.Item1);
+                var bottom = locations.Min(x => x.Key.Item2);
+
+                var region = 0;
+                //loop through everywhere in the grid and work out the distance to each location, then increment the closest one
+                for (int yGrid = bottom; yGrid <= top; yGrid++)
+                {
+                    for (int xGrid = left; xGrid <= right; xGrid++)
+                    {
+                        var totalDistance = FindTotalDistance(xGrid, yGrid);
+                        if (totalDistance < 10000)
+                        {
+                            region++;
+                        }
+                    }
+                }
+                return region;
+            }
+
+            private Tuple<int, int> FindClosest(int x, int y)
+            {
+                var distances = new Dictionary<Tuple<int, int>, int>();
+                foreach(Tuple<int, int> location in locations.Keys)
+                {
+                    var distance = CalculatePointToPointDistance(location.Item1, location.Item2, x, y);
+                    distances.Add(location, distance);
+                }
+                var minDistance = distances.Min(loc => loc.Value);
+                var closestLocations = distances.Where(loc => loc.Value == minDistance);
+                if (closestLocations.Count() == 1)
+                {
+                    return closestLocations.First().Key;
+                } else
+                {
+                    return null ;
+                }
+            }
+
+            private int FindTotalDistance(int x, int y)
+            {
+                var totalDistance = 0;
+                foreach (Tuple<int, int> location in locations.Keys)
+                {
+                    var distance = CalculatePointToPointDistance(location.Item1, location.Item2, x, y);
+                    totalDistance += distance;
+                }
+                return totalDistance;
+            }
+
+            private int CalculatePointToPointDistance(int firstX, int firstY, int secondX, int secondY)
+            {
+                return (Math.Abs(firstX - secondX) + Math.Abs(firstY - secondY));
+            }
+        }
+    }
+}
